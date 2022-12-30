6bb7f94a0acc56a26a628678c7aeb2ca2322a6e9
george ogolla <georgyogolla@gmail.com>
2022-12-30 23:00:11 +0300

    debugging call stack using dev tools

--- asynchronousProgamming/callStack.js
+++ asynchronousProgamming/callStack.js
@@ -1,11 +1,11 @@
 // CALL STACK
-// The mechanism the js interpreter uses to keep track of its place in a scipt that calls multiple functions.
+// The mechanism the js interpreter uses to keep track of its place in a script that calls multiple functions.
 
-//How JS "Knows" what function is currently being run and functions are called fom within that function . 
+//How JS "Knows" what function is currently being run and functions are called from within that function . 
 
 // How the CALL STACK works
 // - When a script calls a function , the interpreter adds it to the call stack and then starts carrying out the function .  
 
 // Any functions that are called by that function are added to the call stack further up, and run where their calls are reached . 
 
-// When the curent function is finished , the interpreter takes it off the stack an resumes execution where it left off in the last code listing . 
+// When the curent function is finished , the interpreter takes it off the stack and resumes execution where it left off in the last code listing . 
